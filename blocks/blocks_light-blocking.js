2bb5da1624f006e828b729ce9306e07b49ceecd2
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 11:21:36 +0700

    add generator

--- blocks/blocks_light-blocking.js
+++ blocks/blocks_light-blocking.js
@@ -1,15 +1,21 @@
-Blockly.Blocks['ky_010_light_blocking_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-010 LIGHT BLOCKING");
-    this.appendValueInput("KY_010_TAP_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_010_light_blocking_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-010 LIGHT BLOCKING");
+      this.appendDummyInput()
+        .appendField("PIN")
+        .appendField(
+          new Blockly.FieldTextInput("0"),
+          "KY_010_LIGHT_BLOCKING_PIN"
+        )
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_010_LIGHT_BLOCKING_VALUE"),
+          "KY_010_LIGHT_BLOCKING_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_light-blocking.js
+++ blocks/generators_light-blocking.js
@@ -1,6 +1,20 @@
-Blockly.JavaScript['ky_010_light_blocking_block'] = function(block) {
-  var value_ky_010_tap_pin = Blockly.JavaScript.valueToCode(block, 'KY_010_TAP_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_010_light_blocking_block"] = function(block) {
+    var text_ky_010_light_blocking_pin = block.getFieldValue(
+      "KY_010_LIGHT_BLOCKING_PIN"
+    );
+    var variable_ky_010_light_blocking_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_010_LIGHT_BLOCKING_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+	int KY_010_LIGHT_BLOCKING_PIN = ${text_ky_010_light_blocking_pin}; // define photo interrupter signal pin
+	#END
+	#SETUP
+		pinMode(KY_010_LIGHT_BLOCKING_PIN, INPUT); //photo interrupter pin as input
+	#END
+	${variable_ky_010_light_blocking_value}=digitalRead(KY_010_LIGHT_BLOCKING_PIN);
+    `;
+    return code;
+  };
